c0a71eec935015f3ebf1adb4830b7bdf6653d28a
Ben <ben@bromleycroft.co.uk>
2018-01-28 22:10:47 +0000

    Added Lobby
    I’ve added a lobby.html page. This communicates currently connected
    players and game settings to the page for the user to view via web
    sockets to give real time feedback of who is connected.
    I’ve used a different namespace (channel) for the web socket
    communications called ‘lobbu’ to make it easier to differentiate
    between this and other game web socket namespaces.
    
    I’ve also now included index.html which will be the first page the user
    sees when they connect. I have added the required menu and username
    input field, which is sent to the server by POST and set as a session
    variable. This will allow me to keep track of which user is which.
    I have also added a function to instantiate a user object at this point
    in the name of the user, which i will use later. I have added this
    because i need to keep track of all my users settings separately.
    
    Finally I have added a CONFIG.txt file to the server folder. This will
    allow an admin to change essential server settings with ease before the
    server is started. I have put it in a text file because it is easier
    and safer to edit than the code for new users, and will be in a
    familiar format.

---
+++ client/resources/js/anykey.js
@@ -0,0 +1,24 @@
+var menu_flag = 0;
+
+$(document).on('keyup',function(evt) {
+  if (menu_flag == 0){
+  	if (evt.keyCode == 8||evt.keyCode == 9||evt.keyCode == 13||evt.keyCode == 16||evt.keyCode == 17||evt.keyCode == 18||evt.keyCode == 19||evt.keyCode == 20||evt.keyCode == 27||evt.keyCode == 33||evt.keyCode == 34||evt.keyCode == 35||evt.keyCode == 36||evt.keyCode == 37||evt.keyCode == 38||evt.keyCode == 39||evt.keyCode == 40||evt.keyCode == 45||evt.keyCode == 46||evt.keyCode == 48||evt.keyCode == 49||evt.keyCode == 50||evt.keyCode == 51||evt.keyCode == 52||evt.keyCode == 53||evt.keyCode == 54||evt.keyCode == 55||evt.keyCode == 56||evt.keyCode == 57||evt.keyCode == 65||evt.keyCode == 66||evt.keyCode == 67||evt.keyCode == 68||evt.keyCode == 69||evt.keyCode == 70||evt.keyCode == 71||evt.keyCode == 72||evt.keyCode == 73||evt.keyCode == 74||evt.keyCode == 75||evt.keyCode == 76||evt.keyCode == 77||evt.keyCode == 78||evt.keyCode == 79||evt.keyCode == 80||evt.keyCode == 81||evt.keyCode == 82||evt.keyCode == 83||evt.keyCode == 84||evt.keyCode == 85||evt.keyCode == 86||evt.keyCode == 87||evt.keyCode == 88||evt.keyCode == 89||evt.keyCode == 90||evt.keyCode == 91||evt.keyCode == 92||evt.keyCode == 93||evt.keyCode == 96||evt.keyCode == 97||evt.keyCode == 98||evt.keyCode == 99||evt.keyCode == 100||evt.keyCode == 101||evt.keyCode == 102||evt.keyCode == 103||evt.keyCode == 104||evt.keyCode == 105||evt.keyCode == 016||evt.keyCode == 107||evt.keyCode == 109||evt.keyCode == 110||evt.keyCode == 111||evt.keyCode == 112||evt.keyCode == 113||evt.keyCode == 114||evt.keyCode == 115||evt.keyCode == 116||evt.keyCode == 117||evt.keyCode == 118||evt.keyCode == 119||evt.keyCode == 120||evt.keyCode == 121||evt.keyCode == 122||evt.keyCode == 123||evt.keyCode == 144||evt.keyCode == 145||evt.keyCode == 186||evt.keyCode == 187||evt.keyCode == 188||evt.keyCode == 189||evt.keyCode == 190||evt.keyCode == 191||evt.keyCode == 192||evt.keyCode == 219||evt.keyCode == 220||evt.keyCode == 221||evt.keyCode == 222) {
+
+    	$('#menu').show();
+ 		$('#PAKTC').hide();
+ 		menu_flag = 1;
+  	};
+  };
+});
+
+
+function join() {
+	$('#menu').hide();
+	$('#join-menu').show();
+};
+function host() {
+	alert('host');
+};
+function about() {
+	alert('host');
+};
--- server/app.py
+++ server/app.py
@@ -3,17 +3,121 @@ import uuid
 import datetime
 import os
 import io
-from flask import Flask, render_template, send_file
+import errno
+from flask import Flask, render_template, send_file, request, session, redirect, render_template_string
 from flask_socketio import SocketIO, emit, send
 from threading import Thread
 
+header = '''
+<head>
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/1.4.8/socket.io.min.js"></script>
+</head>
+<body>
+<script type="text/javascript">
+$(document).ready(function() {
+  var lobby = io.connect('http://127.0.0.1:5000/lobbu');
+
+
+  lobby.on('connect', function() {
+    lobby.send('hi');
+
+
+  });
+
+  lobby.on('message', function(msg) {
+  	var user_table = '<table style="border: 1px solid #000000">';
+  	for (var i=1; i <= msg.USER_LIST.length; i++) {
+  		user_table = user_table + '<tr><td>'+msg.USER_LIST[i-1]+'</td></tr>';
+  	}
+  	user_table = user_table + '</table>';
+    document.getElementById('party').innerHTML = user_table;
+    lobby.send('hi');
+  });
+});
+</script>
+<div id='party'></div>
+</body>
+
+'''
+
+CONFIG = {
+  'JSON_AS_ASCII': True,
+  'USE_X_SENDFILE': False, 
+  'SESSION_COOKIE_PATH': None, 
+  'SESSION_COOKIE_DOMAIN': None, 
+  'SESSION_COOKIE_NAME': 'session', 
+  'DEBUG': False, 
+  'LOGGER_HANDLER_POLICY': 'always', 
+  'LOGGER_NAME': None, 
+  'SESSION_COOKIE_SECURE': False, 
+  'SECRET_KEY': None, 
+  'EXPLAIN_TEMPLATE_LOADING': False, 
+  'MAX_CONTENT_LENGTH': None, 
+  'PROPAGATE_EXCEPTIONS': None, 
+  'APPLICATION_ROOT': None, 
+  'SERVER_NAME': None, 
+  'PREFERRED_URL_SCHEME': 'http', 
+  'JSONIFY_PRETTYPRINT_REGULAR': True, 
+  'TESTING': False, 
+  'PERMANENT_SESSION_LIFETIME': datetime.timedelta(31), 
+  'TEMPLATES_AUTO_RELOAD': None, 
+  'TRAP_BAD_REQUEST_ERRORS': False, 
+  'JSON_SORT_KEYS': True, 
+  'JSONIFY_MIMETYPE': 'application/json',
+  'SESSION_COOKIE_HTTPONLY': True, 
+  'SEND_FILE_MAX_AGE_DEFAULT': datetime.timedelta(0, 43200), 
+  'PRESERVE_CONTEXT_ON_EXCEPTION': None, 
+  'SESSION_REFRESH_EACH_REQUEST': True, 
+  'TRAP_HTTP_EXCEPTIONS': False
+}
+
+GAME = {
+  'GAME_NAME': None,
+  'DESCRIPTION': None,
+  'DIFFICULTY': 0,
+  'ONLINE_MODE': True,
+  'WHITE_LIST': False,
+  'BLACK_LIST': False,
+  'CHEATS': False,
+  'MAX_GAME_LENGTH': None,
+  'BANNED_USERNAMES': False,
+  'SCINARIO': False,
+  'ACTIVE': False,
+  'USER_LIST': []
+}
+
+users = []
+
+class User:
+  def __init__(self, name):
+    self.name = name
+
+
+# MAKE CONFIG FILE
+if os.path.exists('CONFIG.txt') == True:
+  with open('CONFIG.txt', 'r') as f:
+    for line in f.readlines():
+      line_split = line.strip().split("=")
+      CONFIG[line_split[0].strip()] = line_split[1].strip()
+    f.close()
+else:
+  with open('CONFIG.txt', 'w') as f:
+    for key in CONFIG:
+      f.write(key+' = '+str(CONFIG[key])+'\n')
+    f.close()
+
 app = Flask(__name__, template_folder='../client')
 app.config['SECRET_KEY'] = 'secret'
-app.config['DEBUG'] = True
+
+app.config['DEBUG'] = CONFIG['DEBUG']
+
 #app.config['APPLICATION_ROOT'] = '../client'
 for i in app.config: print(i, app.config[i])
 
 socketio = SocketIO(app)
+
 
 data = {
   'time' : '',    #current Time
@@ -24,9 +128,34 @@ data = {
 }
 
 start = datetime.datetime.now()
+
+#================== APP ROUTES - FLASK =================================
+
 @app.route('/')
 def index():
-  return render_template('ui.html')
+  return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+  if request.method == "POST":
+    session['username'] = request.form['username']
+    username = session['username']
+    
+    exec(str(username)+"=User('"+str(username)+"')")
+    
+    print(session['username'])
+    exec("print("+str(username)+".name)")
+
+    GAME['USER_LIST'].append(username)
+    print(session)
+    return redirect('/lobby')
+  return redirect(url_for('/'))
+
+@app.route('/lobby')
+def lobby():
+  if session['username'] != None:
+    #return render_template_string(header)
+    return render_template('lobby.html')
 
 @app.route('/resources/img/<path:path>')
 def img_route(path):
@@ -46,6 +175,7 @@ def route(path):
   else:
     return "ERROR 404: "+str(path)+" doesn't exist"
 
+#================== APP ROUTES - SOCKETIO ==============================
 
 @socketio.on('message')
 def handle_message(message):
@@ -59,6 +189,10 @@ def handle_message(message):
   data['angle'][0] = data['angle'][0]+0.01
   send(data)
 
+
+@socketio.on('message', namespace='/lobbu')
+def handle_lobby_message(message):
+  send(GAME)
 
 if __name__ == '__main__':
   socketio.run(app)
