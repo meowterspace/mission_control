22694e8f53f31aa51160058a717a8a05e79b50f6
Ben <ben@bromleycroft.co.uk>
2018-04-01 13:23:40 +0100

    Update numerical figure selector
    i’ve updated the NFS UI so that the javascript function is generic and
    applies to any function. This is important because I don’t have to
    re-write the function many times over and is reusable.
    This can be seen in the make() function.
    
    It can be altered by passing in a list of the html IDs that it should
    be altering, the value to change it to and where the decimal point
    should lie.

--- UI/numerical figure selector/js/index.js
+++ UI/numerical figure selector/js/index.js
@@ -1,17 +1,20 @@
 var value = 25.3;
 
 
-function make(value){
+function make(id, value, dp){ // List of IDs for values, value, decimal point position in array
+	value = Math.round(value*100)/100;
+	console.log(value);
 	value = value.toString();
 	value = value.split('');
-	document.getElementById('thm1').innerHTML = value[0];
-	document.getElementById('thm2').innerHTML = value[1];
-	document.getElementById('thm3').innerHTML = value[2];
-	document.getElementById('thm4').innerHTML = value[3];
+	for (var i=0; i<id.length; i++){
+		if (value[i] == null){ value[i] = '0';};
+		if (value[dp] == null){ value[dp] = '.';};
+		document.getElementById(id[i]).innerHTML = value[i];
+	};
 };
 
 function change(power, direction){
   value = value + (1*power*direction);
   console.log(value);
-  make(value);
+  make(['thm1', 'thm2', 'thm3', 'thm4'], value, 2); 
 };
